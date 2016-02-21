ee6681ce243c30e2134cdb6911654e9a2d937678
MarcoQin <qyyfy2009@163.com>
2016-02-21 15:05:23 +0800

    new signals

--- main.c
+++ main.c
@@ -1,10 +1,23 @@
 #include <gtk/gtk.h>
 
-GObject *file_chooser;
 void show_file_dialog(GtkButton *button,
-                       gpointer   user_data)
+                       gpointer   file_chooser)
 {
     gtk_widget_show_all (GTK_WIDGET(file_chooser));
+}
+
+void hide_file_dialog(GtkButton *button,
+                        gpointer file_chooser)
+{
+    gtk_widget_hide (GTK_WIDGET(file_chooser));
+}
+
+void hide_file_dialog_2 (GtkDialog *arg0,
+                        gpointer file_chooser)
+{
+    g_print("escape pressed");
+    gtk_widget_hide (GTK_WIDGET(file_chooser));
+    g_print("escape pressed end");
 }
 
 int main(int argc, char *argv[])
@@ -12,6 +25,7 @@ int main(int argc, char *argv[])
     GtkBuilder *builder;
     GObject *button;
     GObject *window;
+    GObject *file_chooser;
     gtk_init (&argc, &argv);
     builder = gtk_builder_new ();
     gtk_builder_add_from_file (builder, "cplayer.ui", NULL);
@@ -19,7 +33,11 @@ int main(int argc, char *argv[])
     g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
     button = gtk_builder_get_object (builder, "button5");
     file_chooser = gtk_builder_get_object (builder, "filechooserdialog1");
-    g_signal_connect (button, "clicked", G_CALLBACK (show_file_dialog), NULL);
+    g_signal_connect (button, "clicked", G_CALLBACK (show_file_dialog), file_chooser);
+    button = gtk_builder_get_object (builder, "button8");
+    g_signal_connect (button, "clicked", G_CALLBACK (hide_file_dialog), file_chooser);
+    g_signal_connect_swapped (file_chooser, "response", G_CALLBACK (gtk_widget_destroy), file_chooser);
+    g_signal_connect_swapped (file_chooser, "close", G_CALLBACK (hide_file_dialog_2), file_chooser);
     gtk_main ();
     return 0;
 }
