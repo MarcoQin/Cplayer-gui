72999a009634968e4b9936d8457f8623841a3fa5
MarcoQin <qyyfy2009@163.com>
2016-02-24 11:43:17 +0800

    add remove func

--- main.c
+++ main.c
@@ -21,9 +21,10 @@ void add_song(gpointer file_path, gpointer liststore) {
     GtkTreeIter iter;
     gint n_rows = gtk_tree_model_iter_n_children(liststore, NULL);
     g_print("n_rows%d\n", n_rows);
+    /* TODO: insert into sqlite3 database */
     gtk_list_store_append(liststore, &iter);
     gtk_list_store_set(liststore, &iter, COL_INDEX, (n_rows + 1), COL_NAME,
-                       (gchar *)file_path, -1);
+                       (gchar *)file_path, COL_ID, (n_rows + 1), -1);
 }
 
 void file_activated(GtkFileChooser *chooser, gpointer liststore) {
@@ -44,16 +45,75 @@ void file_chooser_ok_button(GtkButton *button, GObject *user_data) {
     gtk_widget_hide(GTK_WIDGET(file_chooser));
 }
 
+void selection_foreach_fuc( GtkTreeModel *model,
+                            GtkTreePath  *path,
+                            GtkTreeIter  *iter,
+                            GList       **rowref_list) {
+    gint id;
+    gtk_tree_model_get (model, iter, COL_ID, &id, -1);
+    g_print("id is %d\n", id);
+    if (id != 0)
+    {
+        /* TODO: remove from sqlite3 database */
+        GtkTreeRowReference  *rowref;
+        rowref = gtk_tree_row_reference_new(model, path);
+        *rowref_list = g_list_append(*rowref_list, rowref);
+    }
+}
+
+gboolean re_index_func (GtkTreeModel *model,
+                        GtkTreePath  *path,
+                        GtkTreeIter  *iter,
+                        gint *index) {
+    *index += 1;
+    gtk_list_store_set(GTK_LIST_STORE(model), iter, COL_INDEX, *index, -1);
+    return FALSE; /* do not stop walking the store, call us with next row */
+}
+
+void remove_files(GtkButton *button, gpointer *tree_view) {
+    GtkTreeModel *liststore = gtk_tree_view_get_model(GTK_TREE_VIEW(tree_view));
+    GtkTreeSelection *selection = gtk_tree_view_get_selection(GTK_TREE_VIEW(tree_view));
+    GList *rr_list = NULL;    /* list of GtkTreeRowReferences to remove */
+    GList *node;
+    gtk_tree_selection_selected_foreach(selection,
+                                        (GtkTreeSelectionForeachFunc) selection_foreach_fuc,
+                                        &rr_list);
+    for (node = rr_list; node != NULL; node = node->next)
+    {
+        GtkTreePath *path;
+        path = gtk_tree_row_reference_get_path((GtkTreeRowReference *)node->data);
+        if (path)
+        {
+            GtkTreeIter iter;
+            if (gtk_tree_model_get_iter(GTK_TREE_MODEL(liststore), &iter, path))
+            {
+                gtk_list_store_remove(GTK_LIST_STORE(liststore), &iter);
+            }
+        }
+    }
+    /* re-render all row's index */
+    gint index = 0;
+    gtk_tree_model_foreach(GTK_TREE_MODEL(liststore),
+            (GtkTreeModelForeachFunc) re_index_func,
+            &index);
+
+    g_list_foreach(rr_list, (GFunc) gtk_tree_row_reference_free, NULL);
+    g_list_free(rr_list);
+}
+
 int main(int argc, char *argv[]) {
     GtkBuilder *builder;
     GObject *button;
     GObject *window;
     GObject *liststore;
     GObject *file_chooser_dialog;
+    GObject *treeview;
     gtk_init(&argc, &argv);
     builder = gtk_builder_new();
     gtk_builder_add_from_file(builder, "cplayer.ui", NULL);
     window = gtk_builder_get_object(builder, "window1");  /* main window */
+
+    treeview = gtk_builder_get_object(builder, "treeview1");
 
     liststore = gtk_builder_get_object(builder, "liststore1");  /* liststore */
 
@@ -87,6 +147,9 @@ int main(int argc, char *argv[]) {
     g_object_set_data(G_OBJECT(w), "file_chooser", file_chooser_dialog);
     g_object_set_data(G_OBJECT(w), "liststore", liststore);
     g_signal_connect(button, "clicked", G_CALLBACK(file_chooser_ok_button), w);
+
+    button = gtk_builder_get_object(builder, "button6");  /* "-"(remove files) button */
+    g_signal_connect(button, "clicked", G_CALLBACK(remove_files), treeview);
 
     gtk_main();
     return 0;
