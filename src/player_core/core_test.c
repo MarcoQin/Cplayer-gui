c0b86e67d80429f54b58c9eea7db8c18fbf24dab
MarcoQin <qyyfy2009@163.com>
2016-03-15 12:14:05 +0800

    add test

--- src/player_core/core_test.c
+++ src/player_core/core_test.c
@@ -13,8 +13,20 @@ int main(int argc, char *argv[])
     // Register all formats and codecs
     global_init();
     CPlayer *cp = cp_load_file(argv[1]);
-    SDL_Delay(10000);
-    cp = cp_load_file(argv[2]);
+    SDL_Delay(1000);
+    cp_seek_audio(50);
+    /* SDL_Delay(3000); */
+    while (1) {
+        if (!cp_is_stopping()) {
+            SDL_Delay(500);
+            continue;
+        } else {
+            cp = cp_load_file(argv[2]);
+            break;
+        }
+    }
+    /* cp = cp_load_file(argv[2]); */
+
 
     /* CPlayer *cp = player_create(); */
     /* if (!cp) { */
