bc0ddf9978d85e73732ef4a3bf2546228833118e
MarcoQin <qyyfy2009@163.com>
2016-03-16 09:36:16 +0800

    utils add extract meta data

--- src/utils.c
+++ src/utils.c
@@ -116,3 +116,51 @@ void song_time_to_str(char *s, double total_length, double current_pos) {
     sprintf(label, "%.2d:%.2d / %.2d:%.2d", current_min, current_sec, total_min, total_sec);
     strcpy(s, label);
 }
+
+int extract_meta_data(char *file_path, SongInfo *info) {
+    AVFormatContext *fmt_ctx = NULL;
+    AVDictionaryEntry *tag = NULL;
+    int ret, i;
+
+    info->title = NULL;
+    info->album = NULL;
+    info->artist = NULL;
+    info->genre = NULL;
+    info->track = NULL;
+    info->date = NULL;
+
+    av_register_all();
+    if ((ret = avformat_open_input(&fmt_ctx, file_path, NULL, NULL)))
+        return ret;
+
+    while ((tag = av_dict_get(fmt_ctx->metadata, "", tag, AV_DICT_IGNORE_SUFFIX))) {
+        printf("%s=%s\n", tag->key, tag->value);
+        char key[50];
+        strcpy(key, tag->key);
+        for(i = 0; key[i]; i++){
+          key[i] = tolower(key[i]);
+        }
+        if (strcmp(key, "title") == 0) {
+            info->title = (char *) malloc(strlen(tag->value)+1);
+            strcpy(info->title, tag->value);
+        } else if (strcmp(key, "album") == 0) {
+            info->album = (char *) malloc(strlen(tag->value)+1);
+            strcpy(info->album, tag->value);
+        } else if (strcmp(key, "artist") == 0) {
+            info->artist = (char *) malloc(strlen(tag->value)+1);
+            strcpy(info->artist, tag->value);
+        } else if (strcmp(key, "genre") == 0) {
+            info->genre = (char *) malloc(strlen(tag->value)+1);
+            strcpy(info->genre, tag->value);
+        } else if (strcmp(key, "track") == 0) {
+            info->track = (char *) malloc(strlen(tag->value)+1);
+            strcpy(info->track, tag->value);
+        } else if (strcmp(key, "date") == 0) {
+            info->date = (char *) malloc(strlen(tag->value)+1);
+            strcpy(info->date, tag->value);
+        }
+    }
+
+    avformat_close_input(&fmt_ctx);
+    return 0;
+}
--- src/utils.h
+++ src/utils.h
@@ -3,6 +3,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <libavformat/avformat.h>
+#include <ctype.h>
+#include <libavutil/dict.h>
+#include "db.h"
 
 char *parse_p(char *s);
 int index_of(char[], char[]);
@@ -10,4 +14,5 @@ char *extract_file_name(char *path);
 int extract_song_id(const char *name);
 char *merge_str(char *base, char *middle, char *tail);
 void song_time_to_str(char *s, double total_length, double current_pos);
+int extract_meta_data(char *file_path, SongInfo *info);
 #endif
